5e211128ec8e05e0d357812e93bcf7a835ab5fd4
AlexARNcode <alexarnout@gmail.com>
2021-04-19 18:32:37 +0200

    add getScreenResolution() OK

--- app/jsFunctions.js
+++ app/jsFunctions.js
@@ -1,3 +1,22 @@
 // Get client local time in EN format;
-const localTime = new Date().toLocaleTimeString("en-US");
+function getLocalTime() {
+    const localTime = new Date().toLocaleTimeString("en-US");
 
+    return localTime;
+}
+
+console.log(localTime);
+
+// Get client screen resolution
+function getScreenResolution() {
+    const screenWidth = screen.width;
+    const screenHeight = screen.height;
+
+    resolutionArr = {
+        'width' : screenWidth,
+        'height' : screenHeight,
+    }
+    return resolutionArr;
+}
+
+console.log(getScreenResolution());
