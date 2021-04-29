f666f7a5f5320941e7471823a8dcbb2cca2a6713
AlexARNcode <alexarnout@gmail.com>
2021-04-29 19:35:33 +0200

    WIP geo2ip implementation

--- app/index.php
+++ app/index.php
@@ -1,6 +1,32 @@
 <?php
 require_once('./functions/functions.php');
+require_once '../vendor/autoload.php';
+use GeoIp2\Database\Reader;
+
+$reader = new Reader('./geo2ip/GeoLite2-City.mmdb');
+
+// Replace "city" with the appropriate method for your database, e.g.,
+// "country".
+$record = $reader->city('91.162.88.86');
+
+print($record->country->isoCode . "\n"); // 'US'
+print($record->country->name . "\n"); // 'United States'
+print($record->country->names['zh-CN'] . "\n"); // '美国'
+
+print($record->mostSpecificSubdivision->name . "\n"); // 'Minnesota'
+print($record->mostSpecificSubdivision->isoCode . "\n"); // 'MN'
+
+print($record->city->name . "\n"); // 'Minneapolis'
+
+print($record->postal->code . "\n"); // '55455'
+
+print($record->location->latitude . "\n"); // 44.9733
+print($record->location->longitude . "\n"); // -93.2323
+
+print($record->traits->network . "\n"); // '128.101.101.101/32'
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="fr">
